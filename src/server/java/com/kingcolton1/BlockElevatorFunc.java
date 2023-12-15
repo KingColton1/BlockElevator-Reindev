a2b2ad244a9936a6459ee29326ed7834a33215e4
kingcolton <coltonbailiff@yahoo.com>
2023-12-15 16:04:43 -0500

    minor fix

--- src/server/java/com/kingcolton1/BlockElevatorFunc.java
+++ src/server/java/com/kingcolton1/BlockElevatorFunc.java
@@ -5,7 +5,10 @@ import net.minecraft.src.game.block.Block;
 import net.minecraft.src.game.block.Material;
 import net.minecraft.src.game.entity.player.EntityPlayerMP;
 import net.minecraft.src.game.level.World;
+import net.minecraft.src.game.nbt.PlayerNBTManager;
 import net.minecraft.src.server.physics.AxisAlignedBB;
+import net.minecraft.src.server.player.PlayerController;
+import net.minecraft.src.server.player.PlayerManager;
 
 public class BlockElevatorFunc extends Block {
     public BlockElevatorFunc(int id, Material material) {
@@ -23,7 +26,7 @@ public class BlockElevatorFunc extends Block {
 
     public static Block blockName;
 
-    public static void jump(World world, int x, int y, int z, EntityPlayerMP player){
+    public static void jump(World world, int x, int y, int z, EntityPlayerMP player) {
 		for (int y2 = y + 1; y2 < 255; y2++) {
             if (world.getBlockMaterial(x, y, z) instanceof BlockElevatorFunc) {
                 player.playerNetServerHandler.teleportTo(x + 0.5, y2 + 1, z + 0.5, player.rotationYaw, player.rotationPitch);
@@ -36,7 +39,11 @@ public class BlockElevatorFunc extends Block {
 		}
 	}
 
-    public static void sneak(World world, int x, int y, int z, EntityPlayerMP player){
+    public static void sneak(World world, int x, int y, int z, EntityPlayerMP player) {
+        if (!player.isSneaking()) {
+            return;
+        }
+
 		for (int y2 = y - 1; y2 > 0; y2--) {
 			if (world.getBlockMaterial(x, y2, z) instanceof BlockElevatorFunc) {
 				player.playerNetServerHandler.teleportTo(x + 0.5, y2 + 1, z + 0.5, player.rotationYaw, player.rotationPitch);
